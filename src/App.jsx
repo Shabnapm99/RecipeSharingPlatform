f846d4c6de45126af68300494bdc0b4afdb5f4e8
Shabna P M <pmshabnashareefa@gmail.com>
2025-12-17 16:54:58 +0300

    created basic project structure and added dependencies

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+
+        <h1 className='text-lg font-bold'>Recipe sharing app</h1>
+        
+      </div>
+      
+    </>
+  )
+}
+
+export default App
